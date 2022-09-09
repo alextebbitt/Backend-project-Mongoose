f78ee34a01acf4bd6ca4fe4cbbebec4ab9a9cf1e
alextebbitt <alex.tebbitt87@gmail.com>
2022-09-09 16:02:02 +0200

    password hashed and hidden from login

--- routes/auth.js
+++ routes/auth.js
@@ -1,23 +1,43 @@
 const { Router } = require("express");
 const User = require("../models/User");
-
+const CryptoJS = require("crypto-js");
+const { PASS_SEC } = require("../config/keys");
 const router = require("express").Router();
 
 //REGISTER
-router.post("/register", async (req,res) => {
+router.post("/register", async (req, res) => {
     const newUser = new User({
         username: req.body.username,
         email: req.body.email,
-        password: req.body.password,
-        
+        password: CryptoJS.AES.encrypt(req.body.password, PASS_SEC).toString(),
+
     });
 
-    try{
+    try {
         const savedUser = await newUser.save()
         res.status(201).json(savedUser)
 
-    } catch(err){
+    } catch (err) {
         res.status(500).json(err);
+    }
+})
+
+//LOGIN
+
+
+router.post("/login", async (req, res) => {
+    try {
+        const user = await User.findOne({ username: req.body.username });
+        !user && res.status(401).json("Wrong credentials")
+
+        const hashedPassword = CryptoJS.AES.decrypt(user.password, PASS_SEC);
+        const originalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
+        originalPassword !== req.body.password && res.status(401).json("Wrong credentials!");
+
+        const { password, ...others } = user._doc;
+        res.status(200).json(others)
+    } catch (err) {
+        res.status(500).json(err)
     }
 })
 
