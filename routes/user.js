f31a1595dc51a82ee58c4b9cda525074c702feda
alextebbitt <alex.tebbitt87@gmail.com>
2022-09-11 18:20:00 +0200

    end points users done

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,10 @@
 const { PASS_SEC } = require("../config/keys");
 const User = require("../models/User");
-const { verifyToken, verifyTokenAndAuthorize, verifyTokenAndAdmin } = require("./verifyToken");
+const {
+    verifyToken,
+    verifyTokenAndAuthorize,
+    verifyTokenAndAdmin
+} = require("./verifyToken");
 
 const router = require("express").Router();
 
@@ -24,11 +28,11 @@ router.put("/:id", verifyTokenAndAuthorize, async (req, res) => {
 
 })
 //DELETE
-router.delete("/:id" , verifyTokenAndAuthorize, async (req, res) => {
+router.delete("/:id", verifyTokenAndAuthorize, async (req, res) => {
     try {
         await User.findByIdAndDelete(req.params.id)
         res.status(200).json("User has been deleted.")
-    } catch(err) {
+    } catch (err) {
         res.status(500).json(err)
     }
 })
@@ -39,6 +43,46 @@ router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
         const user = await User.findById(req.params.id)
         const { password, ...others } = user._doc;
         res.status(200).json(others)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+
+//GET ALL 
+router.get("/", verifyTokenAndAdmin, async (req, res) => {
+    const query = req.query.new
+    try {
+        const users = query
+            ? await User.find().sort({ _id: -1 }).limit(5)
+            : await User.find()
+        res.status(200).json(users)
+    } catch (err) {
+        res.status(500).json(err)
+    }
+})
+
+//GET STATS 
+
+router.get("/stats", verifyTokenAndAdmin, async (req, res) => {
+    const date = new Date();
+    const lastYear = new Date(date.setFullYear(date.getFullYear() - 1))
+
+    try {
+        const data = await User.aggregate([{ $match: { createdAt: { $gte: lastYear } } },
+        {
+            $project: {
+                month: { $month: "$createdAt" },
+            },
+        },
+        {
+            $group: {
+                _id: "$month",
+                total: { $sum: 1 },
+            }
+        }
+        ]);
+        res.status(200).json(data)
     } catch (err) {
         res.status(500).json(err)
     }
