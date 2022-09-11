20fa042eed01cf35b105fd3fa59121a2b5a70ca1
alextebbitt <alex.tebbitt87@gmail.com>
2022-09-11 17:32:38 +0200

    user endpoints

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,6 @@
 const { PASS_SEC } = require("../config/keys");
 const User = require("../models/User");
-const { verifyToken, verifyTokenAndAuthorize } = require("./verifyToken");
+const { verifyToken, verifyTokenAndAuthorize, verifyTokenAndAdmin } = require("./verifyToken");
 
 const router = require("express").Router();
 
@@ -22,6 +22,26 @@ router.put("/:id", verifyTokenAndAuthorize, async (req, res) => {
         res.status(500).json(err);
     }
 
+})
+//DELETE
+router.delete("/:id" , verifyTokenAndAuthorize, async (req, res) => {
+    try {
+        await User.findByIdAndDelete(req.params.id)
+        res.status(200).json("User has been deleted.")
+    } catch(err) {
+        res.status(500).json(err)
+    }
+})
+
+//GET
+router.get("/find/:id", verifyTokenAndAdmin, async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id)
+        const { password, ...others } = user._doc;
+        res.status(200).json(others)
+    } catch (err) {
+        res.status(500).json(err)
+    }
 })
 
 module.exports = router
--- routes/verifyToken.js
+++ routes/verifyToken.js
@@ -6,6 +6,7 @@ const { JWT_SEC } = require("../config/keys");
 const verifyToken = (req,res,next) => {
     const authHeader = req.headers.token
     if(authHeader) {
+        const token = authHeader.split(" ")[1]
 jwt.verify(token, JWT_SEC, (err, user) => {
     if (err) res.status(403).json("Token is not valid");
     req.user = user;
@@ -26,4 +27,14 @@ const verifyTokenAndAuthorize = (req,res,next)=>{
     })
 }
 
-module.exports = { verifyToken, verifyTokenAndAuthorize}
+const verifyTokenAndAdmin = (req, res, next) => {
+    verifyToken(req, res, () => {
+        if (req.user.isAdmin) {
+            next()
+        } else {
+            res.status(403).json("You are not allowed")
+        }
+    })
+}
+
+module.exports = { verifyToken, verifyTokenAndAuthorize, verifyTokenAndAdmin}
