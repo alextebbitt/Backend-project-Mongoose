14ced6cb41570524e6eb8b65c0cbaec8c4ebbed1
alextebbitt <alex.tebbitt87@gmail.com>
2022-09-14 12:49:19 +0200

    stripe implemented

--- routes/stripe.js
+++ routes/stripe.js
@@ -1,4 +1,22 @@
 const router = require("express").Router();
+const { STRIPE_KEY } = require("../config/keys");
+const stripe = require("stripe")(STRIPE_KEY)
 
 
-module.exports = router
+router.post("/payment", (req, res) => {
+    stripe.charges.create(
+        {
+            source: req.body.tokenId,
+            amount: req.body.amount,
+            currency: "usd"
+        }, 
+        (stripeErr, stripeRes) => {
+            if (stripeErr) {
+                res.status(500).json(stripeErr);
+            } else {
+                res.status(200).json(stripeRes)
+            }
+        }
+    );
+})
+    module.exports = router
